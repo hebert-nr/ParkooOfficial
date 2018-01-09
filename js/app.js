8f190ace8b57ec8bf18005132c845538dc509304
hebert-nr <nheb461@ecy.wa.lcl>
2018-01-09 10:05:44 -0800

    paging works

--- js/app.js
+++ js/app.js
@@ -1,13 +1,99 @@
 var myApp = angular.module('parkoo', []);
 
 //pulls the data from our js files
-myApp.controller('MyController', function MyController($scope, $http) {
+myApp.controller('MyController', function MyController($scope, $http, $filter) {
     $http.get("js/data.json").then(function (response) {
         $scope.parks = response.data;
         $scope.parkOrder = "parkName"
-        $scope.display = "10";
+        $scope.display = 25;
+        
+        $scope.sort = {
+        sortingOrder: 'id',
+        reverse: false
+    };
+
+    //here down is for pagination
+    $scope.gap = 5;
+
+    $scope.filteredItems = [];
+    $scope.groupedItems = [];
+    $scope.itemsPerPage = $scope.display;
+    $scope.pagedItems = [];
+    $scope.currentPage = 0;
+    var searchMatch = function (haystack, needle) {
+        if (!needle) {
+            return true;
+        }
+        return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+    };
+
+    // init the filtered items
+    $scope.search = function () {
+        $scope.filteredItems = $filter('filter')($scope.parks, function (item) {
+            for (var attr in item) {
+                if (searchMatch(item[attr], $scope.query))
+                    return true;
+            }
+            return false;
+        });
+        // take care of the sorting order
+        if ($scope.sort.sortingOrder !== '') {
+            $scope.filteredItems = $filter('orderBy')($scope.filteredItems, $scope.sort.sortingOrder, $scope.sort.reverse);
+        }
+        $scope.currentPage = 0;
+        // now group by pages
+        $scope.groupToPages();
+    };
+
+
+    // calculate page in place
+    $scope.groupToPages = function () {
+        $scope.pagedItems = [];
+
+        for (var i = 0; i < $scope.filteredItems.length; i++) {
+            if (i % $scope.itemsPerPage === 0) {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [$scope.filteredItems[i]];
+            } else {
+                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]);
+            }
+        }
+    };
+
+    $scope.range = function (size, start, end) {
+        var ret = [];
+        console.log(size, start, end);
+
+        if (size < end) {
+            end = size;
+            start = size - $scope.gap;
+        }
+        for (var i = start; i < end; i++) {
+            ret.push(i);
+        }
+        console.log(ret);
+        return ret;
+    };
+
+    $scope.prevPage = function () {
+        if ($scope.currentPage > 0) {
+            $scope.currentPage--;
+        }
+    };
+
+    $scope.nextPage = function () {
+        if ($scope.currentPage < $scope.pagedItems.length - 1) {
+            $scope.currentPage++;
+        }
+    };
+
+    $scope.setPage = function () {
+        $scope.currentPage = this.n;
+    };
+
+    // functions have been describe process the data for display
+    $scope.search();
     });
-    
+
     //used for advanced search (which features)
     $http.get("js/features.json").then(function (response) {
         $scope.feat = response.data;
@@ -16,10 +102,52 @@ myApp.controller('MyController', function MyController($scope, $http) {
     $scope.parkIndex = function (i) {
         $scope.parkDetail = i;
     };
-    
+
     $scope.$on('$destroy', function () {
         window.onbeforeunload = undefined;
     });
+
+
+});
+
+//more pagination 
+
+myApp.$inject = ['$scope', '$filter'];
+myApp.directive("customSort", function() {
+  return {
+    restrict: 'A',
+    transclude: true,
+    scope: {
+      order: '=',
+      sort: '='
+    },
+    template: ' <a ng-click="sort_by(order)" style="color: #555555;">' +
+      '    <span ng-transclude></span>' +
+      '    <i ng-class="selectedCls(order)"></i>' +
+      '</a>',
+    link: function(scope) {
+
+        // change sorting order
+        scope.sort_by = function(newSortingOrder) {
+          var sort = scope.sort;
+
+          if (sort.sortingOrder == newSortingOrder) {
+            sort.reverse = !sort.reverse;
+          }
+
+          sort.sortingOrder = newSortingOrder;
+        };
+
+
+        scope.selectedCls = function(column) {
+          if (column == scope.sort.sortingOrder) {
+            return ('icon-chevron-' + ((scope.sort.reverse) ? 'down' : 'up'));
+          } else {
+            return 'icon-sort'
+          }
+        };
+      } // end link
+  }
 });
 
 
