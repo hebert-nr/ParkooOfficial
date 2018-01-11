3e481b3aa2b471eacc494f21565d5f39c089f9eb
hebert-nr <nheb461@outlook.com>
2018-01-10 21:33:32 -0800

    cleaned and dirrtied

--- js/app.js
+++ js/app.js
@@ -1,150 +1,15 @@
 var myApp = angular.module('parkoo', []);
 
 //pulls the data from our js files
-myApp.controller('MyController', function MyController($scope, $http, $filter) {
+myApp.controller('MyController', function MyController($scope, $http) {
     $http.get("js/data.json").then(function (response) {
         $scope.parks = response.data;
         $scope.parkOrder = "parkName"
-        
-        
-        
-    //here down is for pagination
-    $scope.pageSizes = [5, 10, 25, 50];
-    $scope.filteredItems = [];
-    $scope.groupedItems = [];
-    $scope.itemsPerPage = 10;
-    $scope.pagedItems = [];
-    $scope.currentPage = 0;
-    var searchMatch = function (haystack, needle) {
-        if (!needle) {
-            return true;
-        }
-        return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
-    };
-
-    // init the filtered items
-    $scope.search = function () {
-        $scope.filteredItems = $filter('filter')($scope.parks, function (item) {
-            for (var attr in item) {
-                if (searchMatch(item[attr], $scope.query))
-                    return true;
-            }
-            return false;
-        });
-        
-        $scope.currentPage = 0;
-        // now group by pages
-        $scope.groupToPages();
-    };
-
-        // show items per page
-    $scope.perPage = function () {
-        $scope.groupToPages();
-    };
-
-    // calculate page in place
-    $scope.groupToPages = function () {
-        $scope.pagedItems = [];
-
-        for (var i = 0; i < $scope.filteredItems.length; i++) {
-            if (i % $scope.itemsPerPage === 0) {
-                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [$scope.filteredItems[i]];
-            } else {
-                $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]);
-            }
-        }
-    };
-
-    $scope.range = function (start, end) {
-        var ret = [];
-        if (!end) {
-            end = start;
-            start = 0;
-        }
-        for (var i = start; i < end; i++) {
-            ret.push(i);
-        }
-        return ret;
-    };
-
-    $scope.prevPage = function () {
-        if ($scope.currentPage > 0) {
-            $scope.currentPage--;
-        }
-    };
-
-    $scope.nextPage = function () {
-        if ($scope.currentPage < $scope.pagedItems.length - 1) {
-            $scope.currentPage++;
-        }
-    };
-
-    $scope.setPage = function () {
-        $scope.currentPage = this.n;
-    };
-
-    // functions have been describe process the data for display
-    $scope.search();
-    });
-    
-    
-    
-
-    //used for advanced search (which features)
-    $http.get("js/features.json").then(function (response) {
-        $scope.feat = response.data;
-    });
-
-    $scope.parkIndex = function (i) {
-        $scope.parkDetail = i;
-    };
-
-    $scope.$on('$destroy', function () {
-        window.onbeforeunload = undefined;
-    });
-
-
+        $scope.display = 10;
+       })
 });
 
-//more pagination 
 
-myApp.$inject = ['$scope', '$filter'];
-myApp.directive("customSort", function() {
-  return {
-    restrict: 'A',
-    transclude: true,
-    scope: {
-      order: '=',
-      sort: '='
-    },
-    template: ' <a ng-click="sort_by(order)" style="color: #555555;">' +
-      '    <span ng-transclude></span>' +
-      '    <i ng-class="selectedCls(order)"></i>' +
-      '</a>',
-    link: function(scope) {
-
-        // change sorting order
-        scope.sort_by = function(newSortingOrder) {
-          var sort = scope.sort;
-
-          if (sort.sortingOrder == newSortingOrder) {
-            sort.reverse = !sort.reverse;
-          }
-
-          sort.sortingOrder = newSortingOrder;
-        };
-
-
-        scope.selectedCls = function(column) {
-          if (column == scope.sort.sortingOrder) {
-            return ('icon-chevron-' + ((scope.sort.reverse) ? 'down' : 'up'));
-          } else {
-            return 'icon-sort'
-          }
-        };
-      } // end link
-  }
-});
 
 
 
