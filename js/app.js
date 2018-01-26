70d62e867580abb395b6dbe3731bf8a44eb89f53
Hebert <nheb461@ecy.wa.lcl>
2018-01-26 10:46:42 -0800

    topull

--- js/app.js
+++ js/app.js
@@ -13,79 +13,9 @@ myApp.factory('parkFac', function ($rootScope, $http) {
 });
 
 
-<<<<<<< HEAD
 
 //main control for app, setting main page information, park details in the sub-pages, and instantiating the map
-myApp.controller('MyController', function MyController($scope, $http, parkFac, NgMap) {
-=======
-myApp.directive('myMap', function() {
-    // directive link function
-    var link = function(scope, element, attrs) {
-        var map, infoWindow;
-        var markers = [];
-        
-        // map config
-        var mapOptions = {
-            center: new google.maps.LatLng(47, -122),
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            scrollwheel: false
-        };
-        
-        // init the map
-        function initMap() {
-            if (map === void 0) {
-                map = new google.maps.Map(element[0], mapOptions);
-            }
-        }    
-        
-        // place a marker
-        function setMarker(map, position, title, content) {
-            var marker;
-            var markerOptions = {
-                position: position,
-                map: map,
-                title: title
-            };
-
-            marker = new google.maps.Marker(markerOptions);
-            markers.push(marker); // add marker to array
-            
-            google.maps.event.addListener(marker, 'click', function () {
-                // close window if not undefined
-                if (infoWindow !== void 0) {
-                    infoWindow.close();
-                }
-                // create new window
-                var infoWindowOptions = {
-                    content: content
-                };
-                infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-                infoWindow.open(map, marker);
-            });
-        }
-        
-        // show the map and place some markers
-        initMap();
-        
-        setMarker(map, new google.maps.LatLng(51.508515, -0.125487), 'London', 'Just some content');
-        setMarker(map, new google.maps.LatLng(52.370216, 4.895168), 'Amsterdam', 'More content');
-        setMarker(map, new google.maps.LatLng(48.856614, 2.352222), 'Paris', 'Text here');
-    };
-    
-    return {
-        restrict: 'A',
-        template: '<div id="gmaps"></div>',
-        replace: true,
-        link: link
-    };
-});
-
-
-
-//main control for app, pulling main page information and park details in the sub-pages
 myApp.controller('MyController', function MyController($scope, $http, parkFac) {
->>>>>>> 3e9408ae24be8e2fb52c4992d452db0dd321e321
     parkFac.getParks().then(function (response) {
         $scope.parks = response.data;
         $scope.parkOrder = "parkName";
@@ -96,9 +26,6 @@ myApp.controller('MyController', function MyController($scope, $http, parkFac) {
         };
     });
     
-    
-
-
     $scope.pageChangeHandler = function (num) {
         console.log('parks page changed to ' + num);
     };
